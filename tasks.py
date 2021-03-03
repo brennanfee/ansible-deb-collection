4d2134687c7a252ad118d4921da016e45c959891
Brennan Fee <brennan+gitlab@mailbots.org>
2021-03-02 23:59:15 -0600

    feat: Switching to python 'invoke' command
    Removed my custom shell scripts and switched to using the python
    'invoke' command.

---
+++ tasks.py
@@ -0,0 +1,18 @@
+from invoke import task
+"""
+To use this script you need to install 'invoke'.  This is best done globally using pip or pipx.
+
+`pipx install invoke`
+
+Afterword, you can execute any of these tasks in the terminal with `inv {task name}`.
+"""
+
+
+@task
+def lint(ctx):
+    ctx.run("poetry run ./molecule/lint")
+
+
+@task(help={"scenario": "The test scenario you wish to run.  Default is the default scenario."})
+def test(ctx, scenario="default"):
+    ctx.run(f"poetry run molecule test -s {scenario}")
